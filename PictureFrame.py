e4b2954cafbbea6124134a9e9c424e35e168dd19
Dag-Are Mathisen <dagga@live.no>
2020-11-30 08:59:04 +0100

    First Commit

---
+++ PictureFrame.py
@@ -0,0 +1,18 @@
+# PictureFrame
+#
+# author: Dag-Are DØving-Mathisen
+# url: https://github.com/DaggaDev/PictureFrame/
+# License: Creative Commons ShareAlike 3.0 (http://creativecommons.org/licenses/by-sa/3.0/)
+#
+#PictureFrame.py: Main File
+#
+##########################
+# Local Config
+##########################
+
+
+
+##########################
+# Import Modules
+##########################
+
